b81b1163e64e2eabc02582d7b3bf05cef56950c2
aninarafath6 <arafathanin6@gmail.com>
2021-05-06 21:38:11 +0530

    scroll bar plugin used

--- home-che-front-end/src/App.js
+++ home-che-front-end/src/App.js
@@ -2,17 +2,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Cart from './components/cart/Cart';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+import React from 'react'
 export default function App() {
+  const scrollableNodeRef = React.createRef();
   return (
-    <div>
+
+    <SimpleBar  autoHide={true} scrollableNodeProps={{ ref: scrollableNodeRef }}>
       <Router>
         <Header />
         <Route   exact path='/' component={Home}/>
         <Route   path='/home' component={Home}/>
         <Route   path='/cart' component={Cart}/>
-
-
       </Router>
-    </div>
+    
+      </SimpleBar>
+
   )
 }
