8d8779b5a38b627fc686631744b1941ce6d4bae4
aninarafath6 <arafathanin6@gmail.com>
2021-05-08 17:05:48 +0530

    backend/sign in route functionalitys

--- backend/helpers/autHelpers.js
+++ backend/helpers/autHelpers.js
@@ -20,8 +20,42 @@ exports.signUp = async (req, res, next) => {
   }
 };
 
-exports.signIn = (req, res, next) => {
-  res.send("this is sign in");
+exports.signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: "please provide email and password",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: "invalid credentials",
+      });
+    }
+
+    const isMatch = await user.matchPasswords(password);
+    if (!isMatch) {
+      res.status(404).json({
+        success: false,
+        error: "invalid credentials",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      token: "ercw344fdsf",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 exports.forgotPassword = (req, res, next) => {
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -34,6 +34,11 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 
+// match password
+UserSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
