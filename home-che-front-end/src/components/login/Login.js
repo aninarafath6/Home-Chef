0e68c2229dd83bb83e963fd73d1c04e33e8beae2
aninarafath6 <arafathanin6@gmail.com>
2021-05-12 10:21:10 +0530

    front-end/sign in form value takig

--- home-che-front-end/src/components/login/Login.js
+++ home-che-front-end/src/components/login/Login.js
@@ -1,27 +1,52 @@
-import React from 'react'
-import { 
-    LoginInput, 
-    LoginOrSignUpContainer,
-    AgreeLink,
-    SubmitButton,
-    ToSignUpContainer,
-    ToSignText,
-    Heading
-    } from './Login.Element';
+import React from "react";
+import useForm from "../../hooks/useForm";
+import {
+  LoginInput,
+  LoginOrSignUpContainer,
+  AgreeLink,
+  SubmitButton,
+  ToSignUpContainer,
+  ToSignText,
+  Heading,
+} from "./Login.Element";
 
 export default function LoginOrSignUp() {
-    
-    return (
-        <LoginOrSignUpContainer>
-            <Heading>Login</Heading>
-            <LoginInput type='email' required placeholder='Email Address' autoFocus={true} />
-            <LoginInput type='password' required placeholder='Password' autoFocus={true} />
-            <SubmitButton type='submit'>Login</SubmitButton>
-            <ToSignUpContainer>
-            <ToSignText>
-            Don't have an account please <AgreeLink to='/signUp'>sign up</AgreeLink> 
-            </ToSignText>
-            </ToSignUpContainer>
-        </LoginOrSignUpContainer>
-    )
+  const [userState, handelUserForm] = useForm({
+    email: "",
+    password: "",
+  });
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(userState);
+  };
+  return (
+    <LoginOrSignUpContainer onSubmit={onSubmitHandler}>
+      <Heading>Login</Heading>
+      <LoginInput
+        type="email"
+        required
+        placeholder="Email Address"
+        autoFocus={true}
+        name="email"
+        onChange={handelUserForm}
+        value={userState.email}
+      />
+      <LoginInput
+        type="password"
+        required
+        placeholder="Password"
+        autoFocus={true}
+        name="password"
+        onChange={handelUserForm}
+        value={userState.password}
+      />
+      <SubmitButton type="submit">Login</SubmitButton>
+      <ToSignUpContainer>
+        <ToSignText>
+          Don't have an account please{" "}
+          <AgreeLink to="/signUp">sign up</AgreeLink>
+        </ToSignText>
+      </ToSignUpContainer>
+    </LoginOrSignUpContainer>
+  );
 }
--- home-che-front-end/src/components/signup/SignUp.js
+++ home-che-front-end/src/components/signup/SignUp.js
@@ -1,37 +1,62 @@
-import React from 'react'
-import useForm from '../../hooks/useForm';
-import { 
-    LoginInput, 
-    LoginOrSignUpContainer,
-    AgreeLink,
-    SubmitButton,
-    ToSignUpContainer,
-    ToSignText,
-    Heading
-    } from './Signup.Element';
+import React from "react";
+import useForm from "../../hooks/useForm";
+import {
+  LoginInput,
+  LoginOrSignUpContainer,
+  AgreeLink,
+  SubmitButton,
+  ToSignUpContainer,
+  ToSignText,
+  Heading,
+} from "./Signup.Element";
 
 export default function LoginOrSignUp() {
-    const [userState,handelUserForm] = useForm({
-        username:'',
-        email:'',
-        password:''
-    })
-   const onSubmitHandler = (e)=>{
-       e.preventDefault();
-       console.log(userState);
-   }
-    return (
-        <LoginOrSignUpContainer onSubmit={onSubmitHandler}>
-            <Heading>Sign Up</Heading>
-            <LoginInput type='text' required placeholder='Username' autoFocus={true} name='username' onChange={handelUserForm} value={userState.username} />
-            <LoginInput type='email' required placeholder='Email Address' name='email' autoFocus={true}  onChange={handelUserForm}  value={userState.email}/>
-            <LoginInput type='password' required placeholder='Password' name='password' autoFocus={true} onChange={handelUserForm}  value={userState.password} />
-            <SubmitButton type='submit'>Login</SubmitButton>
-            <ToSignUpContainer>
-            <ToSignText>
-            Do you have an account please <AgreeLink to='/signIn'>login</AgreeLink> 
-            </ToSignText>
-            </ToSignUpContainer>
-        </LoginOrSignUpContainer>
-    )
+  const [userState, handelUserForm] = useForm({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(userState);
+  };
+  return (
+    <LoginOrSignUpContainer onSubmit={onSubmitHandler}>
+      <Heading>Sign Up</Heading>
+      <LoginInput
+        type="text"
+        required
+        placeholder="Username"
+        autoFocus={true}
+        name="username"
+        onChange={handelUserForm}
+        value={userState.username}
+      />
+      <LoginInput
+        type="email"
+        required
+        placeholder="Email Address"
+        name="email"
+        autoFocus={true}
+        onChange={handelUserForm}
+        value={userState.email}
+      />
+      <LoginInput
+        type="password"
+        required
+        placeholder="Password"
+        name="password"
+        autoFocus={true}
+        onChange={handelUserForm}
+        value={userState.password}
+      />
+      <SubmitButton type="submit">Login</SubmitButton>
+      <ToSignUpContainer>
+        <ToSignText>
+          Do you have an account please{" "}
+          <AgreeLink to="/signIn">login</AgreeLink>
+        </ToSignText>
+      </ToSignUpContainer>
+    </LoginOrSignUpContainer>
+  );
 }
